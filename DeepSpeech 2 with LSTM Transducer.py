b6b7f6a9f3fa172de74cd9e3546308e0d4196097
Morris Xu <philosoengineer@outlook.com>
2020-07-03 17:53:09 +0800

    Update DeepSpeech 2 with LSTM Transducer.py

--- DeepSpeech 2 with LSTM Transducer.py
+++ DeepSpeech 2 with LSTM Transducer.py
@@ -15,15 +15,9 @@ def avg_wer(wer_scores, combined_ref_len):
     return float(sum(wer_scores)) / float(combined_ref_len)
 
 
-# edit distance between two strings using dynamic programming
+# edit distance between two strings
 def _levenshtein_distance(ref, hyp):
-    """Levenshtein distance is a string metric for measuring the difference
-    between two sequences. Informally, the levenshtein disctance is defined as
-    the minimum number of single-character edits (substitutions, insertions or
-    deletions) required to change one word into the other. We can naturally
-    extend the edits to word level when calculate levenshtein disctance for
-    two sentences.
-    """
+    
     m = len(ref)
     n = len(hyp)
 
